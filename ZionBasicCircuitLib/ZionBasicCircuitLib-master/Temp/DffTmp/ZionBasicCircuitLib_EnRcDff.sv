39c72cc0e961b8c1b80faf25ea10fc06cb18a776
gaoyudi <gaoyudi@fpga.(none)>
2019-10-22 17:51:48 +0800

    gaoyudi

--- ZionBasicCircuitLib/ZionBasicCircuitLib-master/Temp/DffTmp/ZionBasicCircuitLib_EnRcDff.sv
+++ ZionBasicCircuitLib/ZionBasicCircuitLib-master/Temp/DffTmp/ZionBasicCircuitLib_EnRcDff.sv
@@ -4,6 +4,11 @@
 // Author      : Wenheng Ma
 // Date        : 2019-07-24
 // Version     : 1.0
+// Parameter   :
+//   WIDTH_IN  - width of input data,input data is range from 0 to 2**WIDTH_IN-1
+//   WIDTH_OUT - width of output data,output data is range from 0 to 2**WIDTH_OUT-1
+//   INI_DATA  - initial value for reset and clear;can be any value,if not injected, default value is '0
+//   RST_CFG   - type of reset,meaning that whether asynchronous or synchronous reset and reset active low or high 
 // Description :
 //   DFF with reset(rst) and enable(iEn).
 //   Reset is configurable:
@@ -24,12 +29,11 @@
 `ifdef ZionBasicCircuitLib_EnRcDff
   `__DefErr__(ZionBasicCircuitLib_EnRcDff)
 `else
-  `define ZionBasicCircuitLib_EnRcDff(UnitName,clk_MT,rst_MT,iEn_MT,iDat_MT,oDat_MT,INI_DATA_MT='0,RST_SYN_MT=RST_SYN,RST_POS_MT=RST_POS)\
+  `define ZionBasicCircuitLib_EnRcDff(UnitName,clk_MT,rst_MT,iEn_MT,iDat_MT,oDat_MT,INI_DATA_MT='0,RST_CFG_MT=4)\
 ZionBasicCircuitLib_EnRcDff #(.WIDTH_IN($bits(iDat_MT)),    \
                                 .WIDTH_OUT($bits(oDat_MT)), \
                                 .INI_DATA(INI_DATA_MT),     \
-                                .RST_SYN(RST_SYN_MT),       \
-                                .RST_POS(RST_POS_MT))       \
+                                .RST_CFG(RST_CFG_MT))       \
                               UnitName(                     \
                                 .clk(clk_MT),               \
                                 .rst(rst_MT),               \
@@ -43,8 +47,7 @@ module ZionBasicCircuitLib_EnRcDff
 #(WIDTH_IN  = "_", //$bits(iDat)// width of input data
   WIDTH_OUT = "_", //$bits(oDat)// width of output data
   INI_DATA  =  '0, //'0         // initial value for reset
-  RST_SYN   =   0, //RST_SYN    // reset type: 0-asynchronous, 1-synchronous
-  RST_POS   =   0  //RST_POS    // reset edge: 0-negedge(active low), 1-posedge(active high)
+  RST_CFG   =   0  //4          // reset type
 )(
   input                        clk,rst,
   input                        iEn, // active high
@@ -52,31 +55,7 @@ module ZionBasicCircuitLib_EnRcDff
   output logic [WIDTH_OUT-1:0] oDat
 );
 
-  `gen_if(RST_SYN==1 && RST_POS==1) begin: Dff_SynPos
-    always_ff@(posedge clk) // DFF with  synchronous reset, and the reset signal is active high.
-      if(rst)
-        oDat <= INI_DATA;
-      else begin
-        if(iEn)
-          oDat <= iDat;
-      end
-  end `gen_elif(RST_SYN==0 && RST_POS==1) begin: Dff_AsynPos
-    always_ff@(posedge clk, posedge rst) // DFF with asynchronous reset, and the reset signal is active high.
-      if(rst)
-        oDat <= INI_DATA;
-      else begin
-        if(iEn)
-          oDat <= iDat;
-      end
-  end `gen_elif(RST_SYN==1 && RST_POS==0) begin: Dff_SynNeg
-    always_ff@(posedge clk) // DFF with  synchronous reset, and the reset signal is active low.
-      if(!rst)
-        oDat <= INI_DATA;
-      else begin
-        if(iEn)
-          oDat <= iDat;
-      end
-  end `gen_elif(RST_SYN==0 && RST_POS==0) begin: Dff_AsynNeg
+  `gen_if(RST_CFG==0) begin: Dff_AsynNeg
     always_ff@(posedge clk, negedge rst) // DFF with asynchronous reset, and the reset signal is active low.
       if(!rst)
         oDat <= INI_DATA;
@@ -84,8 +63,54 @@ module ZionBasicCircuitLib_EnRcDff
         if(iEn)
           oDat <= iDat;
       end
-  end 
-
+  end `gen_elif(RST_CFG==1) begin: Dff_AsynPos
+    always_ff@(posedge clk, posedge rst) // DFF with asynchronous reset, and the reset signal is active high.
+      if(rst)
+        oDat <= INI_DATA;
+      else begin
+        if(iEn)
+          oDat <= iDat;
+      end
+  end `gen_elif(RST_CFG==2) begin: Dff_SynNeg
+    always_ff@(posedge clk) // DFF with  synchronous reset, and the reset signal is active low.
+      if(!rst)
+        oDat <= INI_DATA;
+      else begin
+        if(iEn)
+          oDat <= iDat;
+      end
+  end`gen_elif(RST_CFG==3) begin: Dff_SynPos
+    always_ff@(posedge clk) // DFF with  synchronous reset, and the reset signal is active high.
+      if(rst)
+        oDat <= INI_DATA;
+      else begin
+        if(iEn)
+          oDat <= iDat;
+      end
+  end `gen_elif(RST_CFG==4) begin: Dff_SynPos
+    `ifdef FPGA_PROJECT
+      always_ff@(posedge clk) // In FPGA project, the DFF is recommanded to work with synchronous reset.
+    `elsif ASIC_PROJECT
+      always_ff@(posedge clk, negedge rst) // In ASIC project, the DFF is recommanded to work with asynchronous reset.
+    `else
+      always_ff@(posedge clk, negedge rst) // DFF work with asynchronous reset by default.
+    `endif
+        `ifdef FPGA_PROJECT 
+          if(rst)   // In FPGA project, the reset is recommanded to active high.
+        `elsif ASIC_PROJECT 
+          if(!rst)  // In FPGA project, the reset is recommanded to active low.
+        `else
+          if(!rst)  // By default, the reset is active low.
+        `endif
+            oDat <= INI_DATA;
+          else begin
+            if(iEn)
+              oDat <= iDat;
+          end
+  end //`gen_else begin: Dff_ParamErr
+  //   $error("Parameter Error: RcDff RST_CFG set error!!");
+  // end
+  //end
   // parameter check
   initial begin
     if(WIDTH_IN != WIDTH_OUT) begin
@@ -94,14 +119,8 @@ module ZionBasicCircuitLib_EnRcDff
         $finish;
       `endif
     end
-    if((RST_SYN!=0) && (RST_SYN!=1)) begin
-      $error("Parameter Error: RcDff RST_SYN set error!!");
-      `ifdef CHECK_ERR_EXIT
-        $finish;
-      `endif
-    end
-    if((RST_POS!=0) && (RST_POS!=1)) begin
-      $error("Parameter Error: RcDff RST_POS set error!!");
+    if(!(RST_CFG inside {0,1,2,3,4})) begin
+      $error("Parameter Error: RcDff RST_CFG set error!!");
       `ifdef CHECK_ERR_EXIT
         $finish;
       `endif
